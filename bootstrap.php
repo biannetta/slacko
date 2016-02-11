0d4e5b9c94d6936d715855fceb349b0b92d3abda
Benjamin Iannetta <benjamin.iannetta@gmail.com>
2016-02-10 20:08:18 -0500

    Moving the configuration into seperate file

--- bootstrap.php
+++ bootstrap.php
@@ -1,8 +1,8 @@
 <?php
 	require_once("vendor/kayako-sdk/kyIncludes.php");
 
-	kyConfig::set(new kyConfig( "http://www.prosoftxp.com/support/api/",
-								"8c62d3a9-ad10-21b4-cdda-5cc1da51ba25",
-								"MjE3MWVmOGMtMTg2NS04YWE0LTc1Y2EtOGJhYjM3Yjc5ODJkNGRiY2ZlNzMtYjBkNS01Y2M0LTYxMzEtY2RiMmZhNmE4OGE1")
-	);
+	$config = json_decode(file_get_contents("config/config.json"), true);
+	$api_settings = $config["api"];
+
+	kyConfig::set(new kyConfig($api_settings["url"],$api_settings["key"],$api_settings["secret"]));
 ?>
