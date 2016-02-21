cb450ccc91e90b5e9fa684d73cc16aeeffc5da5b
Benjamin Iannetta <benjamin.iannetta@gmail.com>
2016-02-21 13:02:52 -0500

    Changing response payload

--- index.php
+++ index.php
@@ -5,21 +5,25 @@
 		$ticket = kyTicket::get($ticket_number);
 		$data = array(
 				"text" => $ticket->getSubject(),
+				"response_type" => "in_channel",
 				"attachment" => array(
 					"title" => $ticket->getDisplayId(),
 					"title_link" => "http://prosoftxp.com/support/staff/index.php?/Tickets/Ticket/View/".$ticket->getDisplayId()
 			)
 		);
-		$data = "<http://prosoftxp.com/support/staff/index.php?/Tickets/Ticket/View/".$ticket->getDisplayId()."|".$ticket->getDisplayId()."> ".$ticket->getSubject();
 		return $data;
 	}
 
 	$klein->respond('GET', '/ticket/[i:id]', function($request, $response) {
-		$response->send(json_encode(get_ticket($request->id)));
+		$response->header("content-type","application/json");
+		$response->json(get_ticket($request->id));
+		$response->send();
 	});
 
 	$klein->respond('POST', '/ticket/', function($request, $response) {
-		$response->send(json_encode(get_ticket($request->param("text"))));
+		$response->header("content-type","application/json");
+		$response->json(get_ticket($request->param("text")));
+		$response->send();
 	});
 	$klein->dispatch();
 ?>
