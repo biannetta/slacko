21429b22c3bf5b607ba9636ae81248e95e927673
Benjamin Iannetta <benjamin.iannetta@gmail.com>
2016-02-27 13:20:36 -0500

    Adding in Staff Search

--- bootstrap.php
+++ bootstrap.php
@@ -7,6 +7,13 @@
 	$api_settings = $config["kayako"];
 	$slack_settings = $config["slack"];
 
+	$slack_users = array(
+		"@biannetta" => "Ben",
+		"@tshadd" => "Ty",
+		"@eros" => "Eros",
+		"@joe.muresan" => "Joe"
+	);
+
 	kyConfig::set(new kyConfig($api_settings["url"],$api_settings["key"],$api_settings["secret"]));
 	$klein = new \Klein\Klein();
 ?>
--- index.php
+++ index.php
@@ -11,11 +11,6 @@
 						"title_link" => "http://prosoftxp.com/support/staff/index.php?/Tickets/Ticket/View/".$ticket->getDisplayId(),
 						"fields" => array(
 							array(
-								"title" => "Department",
-								"value" => $ticket->getDepartment(),
-								"short" => "true"
-							),
-							array(
 								"title" => "Creator",
 								"value" => $ticket->getFullName(),
 								"short" => "true"
@@ -32,6 +27,11 @@
 		return $data;
 	}
 
+	function get_staff_member_by_user($username) {
+		$staff = kyStaff::getAll()->filterByUserName($slack_users[$username]);
+		return $staff;
+	}
+
 	$klein->respond('GET', '/ticket/[i:id]', function($request, $response) {
 		$response->header("content-type","application/json");
 		$response->json(get_ticket($request->id));
@@ -39,8 +39,16 @@
 	});
 
 	$klein->respond('POST', '/ticket/', function($request, $response) {
+		$raw_text = $request->param("text");
+		$params = explode(" ", $raw_text);
+
+		if (count($params) == 2) {
+			$staff = get_staff_member_by_user($params[1]);
+			file_put_contents('php://stderr', print_r($staff));
+		}
+
 		$response->header("content-type","application/json");
-		$response->json(get_ticket($request->param("text")));
+		$response->json(get_ticket($params[0]));
 		$response->send();
 	});
 	$klein->dispatch();
